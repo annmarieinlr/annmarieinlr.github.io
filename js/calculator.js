bd8a5ac308e18192fa6e4d1928c5d3dc4a847b6d
annmarieinlr <wor20005@byui.edu>
2020-10-23 16:40:45 -0500

    up

--- js/calculator.js
+++ js/calculator.js
@@ -6,34 +6,50 @@ function calculator() {
     let num1 = parseFloat(document.getElementById('number1').value);
     let num2= parseFloat(document.getElementById('number2').value);
     let myans = parseFloat(document.getElementById('myanswer').value);
-
+    let operator = (document.getElementById('operator').value);
     //Processing:
     //compute the problem
     //use switch case for operator
-
+    
+    let message;
+    let answer;
     switch(operator) {
         case '+':
-            answer = num1 + num2;
+            answer = parseFloat(num1 + num2);
+            if (myans == answer){
+                message = "Correct! Great Job."
+            } else {
+                message = "Incorrect, but you can try again."
+            }
             break;
         case '-':
-            answer = num1 - num2;
+            answer = parseFloat(num1 - num2);
+            if (myans == answer){
+                message = "Correct! Great Job."
+            } else {
+                message = "Incorrect, but you can try again."
+            }
             break;
         case '*':
-            answer = num1 * num2;
+            answer = parseFloat(num1 * num2);
+            if (myans == answer){
+                message = "Correct! Great Job."
+            } else {
+                message = "Incorrect, but you can try again."
+            }
             break;
         case '/':
-            answer = num1/num2;
+            answer = parseFloat(num1/num2);
+            if (myans == answer){
+                message = "Correct! Great Job."
+            } else {
+                message = "Incorrect, but you can try again."
+            }
             break;         
     }
 
    
     //Output:
     //Check their answer
-    document.getElementById('output').innerHTML = 
-        if (myans == answer) {
-            alert("Correct! Great Job!");
-        }
-        else {
-            alert("Incorrect, but you can try again!");
-        }
+    document.getElementById('output').innerHTML = message
 }
